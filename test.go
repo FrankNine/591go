b6b6ffdb8e3b57beca13d8d51337c1ce2c94b362
Chun-Fu Chao <aaefiikmnnnr@gmail.com>
2020-06-09 02:35:02 +0800

    Add constraint

--- test.go
+++ test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"strings"
 
@@ -12,27 +13,34 @@ import (
 
 func main() {
 	options := rent.NewOptions()
+	// https://github.com/neighborhood999/fiveN1-rent-scraper/blob/master/list/url-jump-ip.md
+	options.Region = 1
+	// https://github.com/neighborhood999/fiveN1-rent-scraper/blob/master/list/section.md
+	options.Section = "4,7"
+	options.Kind = 0
+	options.RentPrice = "10000, 40000"
+
 	url, err := rent.GenerateURL(options)
 	if err != nil {
 		log.Fatalf("\x1b[91;1m%s\x1b[0m", err)
 	}
 
 	f := rent.NewFiveN1(url)
-	if err := f.Scrape(1); err != nil {
+	if err := f.Scrape(f.GetTotalPage()); err != nil {
 		log.Fatal(err)
 	}
-
-	json := rent.ConvertToJSON(f.RentList)
-	log.Println(string(json))
 
 	db, err := sql.Open("sqlite3", "./foo.db")
 	checkErr(err)
 
-	stmt, err := db.Prepare("INSERT INTO rentinfo(title, url, address, floor, max_floor) values(?,?,?,?,?)")
+	stmt, err := db.Prepare("INSERT INTO rentinfo(title, url, address, floor, max_floor, is_new, ping, price, rent_type, option_type) values(?,?,?,?,?,?,?,?,?,?)")
 	checkErr(err)
 
-	for _, page := range f.RentList {
+	for p, page := range f.RentList {
+		fmt.Printf("Page: %d", p)
 		for _, r := range page {
+			fmt.Println(r.Title)
+
 			floorWithoutPrefix := strings.TrimPrefix(r.Floor, "樓層：")
 			hasMaxFloor := strings.Contains(floorWithoutPrefix, "/")
 			var floor string
@@ -45,8 +53,7 @@ func main() {
 				floor = floorWithoutPrefix
 				maxFloor = ""
 			}
-
-			_, err := stmt.Exec(r.Title, r.URL, r.Address, floor, maxFloor)
+			_, err := stmt.Exec(r.Title, r.URL, r.Address, floor, maxFloor, r.IsNew, r.Ping, r.Price, r.RentType, r.OptionType)
 			checkErr(err)
 		}
 	}
@@ -55,7 +62,7 @@ func main() {
 }
 
 func checkErr(err error) {
-	if err != nil {
+	if err != nil && !strings.Contains(err.Error(), "UNIQUE") {
 		panic(err)
 	}
 }
