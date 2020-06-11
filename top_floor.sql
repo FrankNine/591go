98c41a2d18387c5cc0e4111d324ea7e17aa0dd90
Chun-Fu Chao <aaefiikmnnnr@gmail.com>
2020-06-11 20:53:06 +0800

    Update top floor query

--- top_floor.sql
+++ top_floor.sql
@@ -1,1 +1,5 @@
-SELECT * FROM rentInfo WHERE floor = max_floor
+SELECT * FROM rentInfo WHERE floor = max_floor AND is_new = 1
+AND title NOT LIKE "%六張犁%" 
+AND title NOT LIKE "%北醫%"
+AND title NOT LIKE "%麟光%"
+AND option_type NOT LIKE "%分租%" 
